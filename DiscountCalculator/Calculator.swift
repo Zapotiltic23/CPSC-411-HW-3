e73a7837af9227484a9755da2765842dc20a63fd
lis meza <lisette_56@lis-mezas-MacBook-Pro.local>
2017-03-20 16:02:34 -0700

    Updated 3/20/2017

--- DiscountCalculator/Calculator.swift
+++ DiscountCalculator/Calculator.swift
@@ -1,10 +1,14 @@
 //
-//  Calculator.swift
-//  DiscountCalculator
+// Calculator.swift
+// Project: discountCalculator
+// Name: Horacio A Sanchez
+// Professor: David McLaren
+// Class: CPSc 411 M 7-10pm
+// Date: March 20, 2017
 //
-//  Created by lis meza on 3/13/17.
-//  Copyright © 2017 Horacio Sanchez. All rights reserved.
-//
+// Description:
+// This program illustrates the use of segues in swift and creation of graphical bar charts used to display results. The program is in essence a calculator that computes discounts and tax to a given price amount. It then displays the price to pay as well as the amount of money saved in both actual sums of money and percentages. Then the user swipes on the screen to display the bar chart that visually displays the calculations.
+//-------------------------------------------------------------------------------------------------------------------------------------------
 
 import UIKit
 
